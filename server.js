a6e47289b6a4cef6f57df756e53ba60506e5c2d1
Sascha van Vliet <saschacharlotte@gmail.com>
2025-03-28 15:56:00 +0100

    #1 Html prototype post
    Je kan nu stekjes opslaan in de database (Directus)!

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ const stekjesResponseJSON = await stekjesResponse.json()
 const plaatjesResponse = await fetch('https://fdnd-agency.directus.app/items/bib_afbeeldingen?filter={%20%22type%22:%20{%20%22_eq%22:%20%22stekjes%22%20}}')
 const plaatjesResponseJSON = await plaatjesResponse.json()
 
+const userId = 6
+
 app.get('/', async function (request, response) {
  response.render('index.liquid', {
   stekjes: stekjesResponseJSON.data,
@@ -27,8 +29,6 @@ app.get('/', async function (request, response) {
 })
 
 app.get('/stekjes', async function (request, response) {
-  // Render index.liquid uit de Views map
-  // Geef hier eventueel data aan mee
 response.render('stekjes.liquid', {
  stekjes: stekjesResponseJSON.data,
  plaatjes: plaatjesResponseJSON.data
@@ -79,6 +79,39 @@ app.post(…, async function (request, response) {
   response.redirect(303, …)
 })
 */
+
+app.post('/stekjes/:id', async function (request, response) {
+  const stekjeId = request.params.id;
+    const userstekjeEntry = await fetch(`https://fdnd-agency.directus.app/items/bib_users_stekjes?filter={"bib_stekjes_id":${stekjeId},"bib_users_id":${userId}}`)
+    const userstekjeEntryJSON = await userstekjeEntry.json()
+
+    if (userstekjeEntryJSON.data.length != 0) {
+      // Delete de boel uit Directus
+      await fetch(`https://fdnd-agency.directus.app/items/bib_users_stekjes/${userstekjeEntryJSON.data[0].id}`, {
+        method: 'DELETE'
+      });
+    } else {
+      const directusResponse = await fetch('https://fdnd-agency.directus.app/items/bib_users_stekjes', {
+        method: 'POST',
+        body: JSON.stringify({
+          bib_stekjes_id: stekjeId, 
+          bib_users_id: 6, 
+        }),
+        headers: {
+          'Content-Type': 'application/json' 
+        }
+      });
+    }
+    response.redirect(303, '/');
+});
+
+app.get('/404', async function (request, response) {
+  response.render('404.liquid')
+})
+
+app.use((req, res, next) => {
+  res.status(404).render('404.liquid')
+})
 
 app.set('port', process.env.PORT || 8888)
 
